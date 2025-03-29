7780675c91420872eb04c256f4898d62b97914c6
cuboid <cubeyyyy@pm.me>
2025-03-29 00:29:50 +0000

    remove commented-out alpha.nvim option, not needed

--- nvim/.config/nvim/lua/plugins/alpha.lua
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -496,7 +496,6 @@ return {
 		}
 
 		dashboard.section.buttons.val = {
-			-- dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
 			dashboard.button("b", "  > Browse files", ":Neotree reveal float<CR>"),
 			dashboard.button("z", "  > Browse Directories", ":Telescope zoxide list<CR>"),
 			dashboard.button("f", "󰈞  > Find file", ":Telescope find_files<CR>"),
