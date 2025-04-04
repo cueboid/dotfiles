e15dacc42f3ad67ff0f2fe3769bfb127b9de5f9f
cuboid <cubeyyyy@pm.me>
2025-04-04 05:10:06 +0100

    make lualine look cool
    and remove old, commented code; not needed

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,20 +1,12 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	kdependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				icons_enabled = false,
+				theme = "auto",
+			},
+		})
+	end,
 }
---[[
-return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "echasnovski/mini.icons" },
-  config = function()
-    require("lualine").setup({
-      options = {
-        icons_enabled = false,
-        theme = "auto",
-        component_separators = "",
-        section_separators = "",
-      },
-    })
-  end,
-}
-]]
